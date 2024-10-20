4019ce77fe9c5a3d20d3e332e83ae094972236db
swati22446 <swati22446@gmail.com>
2024-10-20 19:47:45 +0530

    Ship moving sideways

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ const a=canvas.getContext('2d');
 canvas.width=innerWidth;
 canvas.height=innerHeight;
 
-class Player{
+class Ship{
     constructor() {
         
       
@@ -29,16 +29,88 @@ class Player{
         if(this.image && this.width && this.height)
         a.drawImage(this.image ,this.position.x ,this.position.y, this.width, this.height);
     }
+    update(){
+        if(this.image && this.width && this.height)
+        this.position.x +=this.velocity.x;
+        this.draw();
+        
+        
+    }
 }
 
-const player=new Player();
-player.draw();
+const player=new Ship();
+const keys={
+    a: {
+        pressed:false
+    },
+    d: {
+        pressed:false
+    },
+    space: {
+        pressed:false
+    }
+
+}
 
 function animate(){
     requestAnimationFrame(animate);
     a.fillStyle='black';
     a.fillRect(0 , 0, canvas.width, canvas.height);
     player.draw();
-
+    if (keys.a.pressed && player.position.x >=0){
+        player.velocity.x = -5;
+    }
+    else if(keys.d.pressed && player.position.x +player.width <canvas.width){
+        player.velocity.x=5;
+    }
+    else{
+        player.velocity.x=0;
+    }
+    player.update();
 }
 animate();
+
+window.addEventListener("keydown",(event)=>{
+    switch(event.key){
+        case 'a':
+            console.log('left');
+            keys.a.pressed=true;
+            break;
+        case 'd':
+            console.log('right');
+            player.velocity.x =5;
+            keys.d.pressed=true;
+            break;
+        case 's':
+            console.log('down');
+            break;
+        case 'w':
+            console.log('up');
+            break;
+        case ' ':
+            console.log('space');
+            break;
+    }
+});
+window.addEventListener("keyup",(event)=>{
+    switch(event.key){
+        case 'a':
+            console.log('left');
+            keys.a.pressed=false;
+            break;
+        case 'd':
+            console.log('right');
+            player.velocity.x =5;
+            keys.d.pressed=false;
+            break;
+        case 's':
+            console.log('down');
+            break;
+        case 'w':
+            console.log('up');
+            break;
+        case ' ':
+            console.log('space');
+            break;
+    }
+})
