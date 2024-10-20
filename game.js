6f16b86d6ba94992f0ef6ef93dcdafe1c7deb9f4
swati22446 <swati22446@gmail.com>
2024-10-20 14:37:44 +0530

    Player is made

---
+++ game.js
@@ -0,0 +1,11 @@
+const canvas=document.querySelector('canvas')
+const a=canvas.getContext('2d')
+canvas.width=innerWidth
+canvas.height=innerHeight
+class player{
+    constructor(){
+        this.position={
+            
+        }
+    }
+}
